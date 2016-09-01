aa4c28e1ec0ccd8ac0bb1eac0f7afbd3e22eb140
marcelacoury <marcelacouryp@hotmail.com>
2016-09-01 12:08:54 -0300

    fim

--- aula20160825/cpx3.c
+++ aula20160825/cpx3.c
@@ -1,1 +1,18 @@
+#include<stdio.h>
+#include<complex.h>
+#include<math.h>
+
+int main(){
+    double complex Z1,Z2, raiz;
+    double preal, pimag;
+    printf("Informe a parte real do Z1: ");
+    scanf("%lf", &preal);
+    printf("\nInforme a parte imaginaria do Z1: ");
+    scanf("%lf", &pimag);
+    Z1 = preal + pimag*I;
+    raiz = csqrt(Z1);
+    printf("\nProduto: %lf + %lf i\n", creal(raiz), cimag(raiz));
+    printf("\nProduto: %lf /_ %lf rad\n", cabs(raiz), carg(raiz));
+    return 0;
+}
 
